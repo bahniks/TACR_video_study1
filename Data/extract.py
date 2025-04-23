5b085cc4b07bf1d5ba79d04587131cdc220be3a7
Stepan Bahnik <bahniks@seznam.cz>
2025-04-23 10:24:35 +0200

    Update extract.py

--- Data/extract.py
+++ Data/extract.py
@@ -1,3 +1,6 @@
+#! python3
+# -*- coding: utf-8 -*- 
+
 import os
 
 
@@ -46,14 +49,14 @@ frames = ["Initial",
          ]
 
 read = True
-compute = False
+compute = True
 
 if read:
     for study in studies:
-        with open("{} results.txt".format(study), mode = "w") as f:
+        with open("{} results.txt".format(study), mode = "w", encoding = "utf-8") as f:
             f.write("\t".join(studies[study]))
 
-    with open("Time results.txt", mode = "w") as times:
+    with open("Time results.txt", mode = "w", encoding = "utf-8") as times:
         times.write("\t".join(["id", "order", "frame", "time"]))
 
     files = os.listdir()
@@ -69,13 +72,13 @@ if read:
                 study = line.strip()
                 if line.startswith("time: "):
                     with open("Time results.txt", mode = "a") as times:
-                        print(frames[count-1])
-                        print(line.split()[1])
+                        #print(frames[count-1])
+                        #print(line.split()[1])
                         times.write("\n" + "\t".join([file, str(count), frames[count-1], line.split()[1]]))
                         count += 1
                         continue
                 if study in studies:
-                    with open("{} results.txt".format(study), mode = "a") as results:
+                    with open("{} results.txt".format(study), mode = "a", encoding = "utf-8") as results:
                         for line in datafile:
                             content = line.strip()
                             if not content or content.startswith("time: "):
